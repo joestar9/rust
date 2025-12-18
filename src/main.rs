22dbfbf71dfd91ff474f446b4bdb76f9c1ca6f22
joestar9 <159675199+joestar9@users.noreply.github.com>
2025-12-18 08:53:11 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use anyhow::{Context, Result, anyhow};
 use chrono::Local;
 use rand::prelude::*;
 use reqwest::{Client, Proxy};
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashSet;
 use std::fs::OpenOptions;
